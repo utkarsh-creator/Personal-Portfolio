6456cee57e11635fad8b489569b1f9c274e529e0
Utkarsh <utkarshtyagi959@gmail.com>
2022-04-11 21:58:20 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Script running...")
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    
+})
+
